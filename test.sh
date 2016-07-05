d9aa5f305c33a52c63f9870d0f924c56b308d52f
AlfonsoSuvierri <Alfonso6829@hotmail.com>
2016-07-05 06:01:49 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,8 +4,21 @@ domlogs='/usr/local/apache/domlogs/'$USER; # Just to declare the domlog path.
 ## Start off by initializing the functions
 searchLogs() {
   if [ ! "$search_term" ]
-  then
-    echo "Variable is empty. Continue w/ all.";
+  then ## Start to pull all options for the domlogs
+  d_x=1;
+  echo "Obtaining options for" $USER; echo;
+  
+  for option in "$domlogs"/* 
+    do
+      echo $d_x." $option";
+      eval dom$d_x=$option;
+      d_x=$((d_x + 1));
+    done
+  
+  #echo; echo -ne "Type the number of the path you wish to review: "; read;
+  
+  #domain_toread=$(eval echo "\$dom$REPLY");
+  
   else
     echo "Picked up variable -- $search_term";
   fi
