a9366d6f85973cdb59b308680e21b293dc39479f
AlfonsoSuvierri <Alfonso6829@hotmail.com>
2016-07-07 23:11:16 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -32,7 +32,7 @@ searchLogs() {
         eval dom$d_x=$line;
         domList[$d_x]=$line;
         d_x=$((d_x + 1));
-        #echo ${domList[@]};
+        echo ${domList[@]};
       done
     
     echo ${domList[@]};
